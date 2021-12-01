1d6fe5f9fc23978242a4d69665ac7d59995afa8f
jayashrinandargi <jayashriwit@gmail.com>
2021-11-30 19:47:18 -0500

    First commit V1.0

---
+++ src/GoogleTestDemo/GoogleTest.java
@@ -0,0 +1,43 @@
+package GoogleTestDemo;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class GoogleTest {
+	
+    ChromeDriver driver;
+	
+	String url="https://www.google.com/?gws_rd=ssl";
+	
+	
+	public void invokeBrowser() {
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\suraj\\eclipse-workspace\\libs\\chromedriver.exe");
+		
+	    driver=new ChromeDriver();
+	    
+	    driver.manage().window().maximize();
+	    
+	    driver.manage().deleteAllCookies();
+	    driver.get(url);
+	    
+	}
+	
+	
+	public void CloseBrowser() {
+		
+		//driver.close();  quit() is used to exit the browser, end the session, tabs, pop-ups etc.
+		driver.quit();  //only the window that has focus is closed. 
+		
+	}
+	
+
+	public void SearchGoogle() {
+		
+		WebElement userElement = driver.findElement(By.name("q"));
+		userElement.sendKeys("Months of the year");
+		
+		
+	}
+}
---
+++ src/GoogleTestDemo/MainGoogleTest.java
@@ -0,0 +1,15 @@
+package GoogleTestDemo;
+
+public class MainGoogleTest {
+
+	public static void main(String[] args) {
+
+		GoogleTest gt= new GoogleTest();
+		
+		gt.invokeBrowser();
+		
+		System.out.println("Google Page Opened sucessfully");
+		
+	}
+
+}
